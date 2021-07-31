0789a28af4fdd44a31c48dfe989315be0d63890e
Jan Mewes <jan.mewes@experimental-software.com>
2021-07-31 19:50:12 +0200

    Add unit test for patient creation

--- test/api/patient/patient_service_test.dart
+++ test/api/patient/patient_service_test.dart
@@ -1,11 +1,41 @@
+import 'package:ksch_dart_client/ksch_dart_client.dart';
+import 'package:ksch_dart_client/src/patients/payload.dart';
+import 'package:nock/nock.dart';
+import 'package:registration_desk/api/patient/patient_service.dart';
 import 'package:test/test.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
-  test('Should create patient', () {
+  late PatientServiceImpl patientService;
 
+  setUpAll(nock.init);
+
+  setUp(() {
+    nock.cleanAll();
+
+    patientService = PatientServiceImpl('http://localhost');
   });
 
-  test('Should create patient in case of emergeny', () {
+  test('Should create patient', () {});
 
+  test('Should create patient in case of emergency', () async {
+    var patientId = const Uuid().v4();
+    givenCreatePatientResponse(MinimalPatientResponse(patientId).toJson());
+
+    var result = await patientService.create(null);
+
+    expect(result.id, equals(patientId));
   });
+}
+
+void givenCreatePatientResponse(dynamic body) {
+  nock('http://localhost').post('/api/patients')..reply(200, body);
+}
+
+class MinimalPatientResponse extends PatientResponsePayload {
+  MinimalPatientResponse(String id)
+      : super(
+          id: id,
+          links: Links(self: Link(href: 'http://localhost/api/patients/$id')),
+        );
 }
