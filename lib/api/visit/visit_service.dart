c98c599f2633e160bc56f20718f0cf60bdc5867c
Jan Mewes <jan.mewes@experimental-software.com>
2021-07-31 19:24:50 +0200

    Add service to start visit

--- lib/api/visit/visit_service.dart
+++ lib/api/visit/visit_service.dart
@@ -1,18 +1,33 @@
+import 'package:ksch_dart_client/src/client.dart';
+
 import 'package:uuid/uuid.dart';
 
 import 'visit.dart';
 
 // ignore: one_member_abstracts
 abstract class VisitService {
-  Visit startVisit(String? patientId);
+  Future<Visit> startVisit(String? patientId);
+}
+
+class VisitServiceImpl implements VisitService {
+
+  final KschApi _api;
+
+  VisitServiceImpl(this._api);
+
+  @override
+  Future<Visit> startVisit(String? patientId) {
+    throw UnimplementedError();
+  }
 }
 
 class MockVisitService implements VisitService {
   @override
-  Visit startVisit(String? patientId) {
-    return Visit(
+  Future<Visit> startVisit(String? patientId) {
+    var result = Visit(
       id: const Uuid().v4(),
       patientId: patientId,
     );
+    return Future.sync(() => result);
   }
 }
---
+++ test/api/patient/patient_service_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Should create patient', () {
+
+  });
+
+  test('Should create patient in case of emergeny', () {
+
+  });
+}
