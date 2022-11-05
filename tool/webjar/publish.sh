c33ae059b17de501c1fe83110a88e70e43d688ab
Jan Mewes <jan.mewes@ks-plus.org>
2022-11-05 18:11:01 +0100

    Use script parameters

--- tool/webjar/publish.sh
+++ tool/webjar/publish.sh
@@ -5,39 +5,52 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_ROOT_DIR=${SCRIPT_DIR}/../..
 FLUTTER_BUILD_DIR=${REPO_ROOT_DIR}/build/web
-PUBLISH_MAVEN_LOCAL="no"
-
 
 function usage()
 {
-  echo "Compiles the Flutter Web app, packages it into a JAR files and then publishes it into a Maven repository."
-  echo "./publish.sh"
-  echo "\t-h --help"
-  echo "\t-l --local"
-  echo ""
-}
-# TODO Add support for passing in version number
+    cat <<-END
+usage: publish.sh -v VERSION [-l] [-h]
 
-while [ "$1" != "" ]; do
-  PARAM=`echo $1 | awk -F= '{print $1}'`
-  VALUE=`echo $1 | awk -F= '{print $2}'`
-  case $PARAM in
-    -h | --help)
-      usage
-      exit
+This script compiles the Flutter Web app, packages it
+into a JAR file and then publishes it into a Maven repository.
+
+required arguments:
+  -v    The version number of the published package.
+
+optional arguments:
+  -l    Publish to Maven Local instead of the cloud repository.
+  -h    Show this help message and exit.  
+END
+}
+
+LOCAL_RELEASE="no"
+
+while getopts "h l v:" o; do
+  case "${o}" in
+    v)
+      VERSION=${OPTARG}
       ;;
-    -l | --local)
-      PUBLISH_MAVEN_LOCAL="yes"
+    l)
+      LOCAL_RELEASE="yes"
       ;;
-    *)
-      echo "ERROR: unknown parameter \"$PARAM\""
+    h | *)
       usage
-      exit 1
+      exit 0
       ;;
   esac
-  shift
 done
+shift $((OPTIND-1))
 
+if [[ -z "${VERSION}" ]] ; then
+  echo "Missing required parameter for version."
+  usage
+  exit 1
+fi
+
+which flutter > /dev/null || { echo "ERROR: flutter not installed"; exit 1; }
+which gradle > /dev/null || { echo "ERROR: gradle not installed"; exit 1; }
+
+set -e
 
 cd ${REPO_ROOT_DIR}
 flutter test
@@ -50,11 +63,9 @@ fi
 mkdir -p src/main/resources/META-INF/resources/registration-desk
 cp -r ${FLUTTER_BUILD_DIR}/* src/main/resources/META-INF/resources/registration-desk
 
-PROPS=""
-if [[ "${PUBLISH_MAVEN_LOCAL}" == "yes" ]]; then
-  # TODO set version number as Gradle property
-  gradle publishToMavenLocal
+PROPS="-Pversion=${VERSION}"
+if [[ "${LOCAL_RELEASE}" == "yes" ]]; then
+  gradle publishToMavenLocal $PROPS 
 else
-  # TODO set version number as Gradle property
-  gradle publish
+  echo "TODO: gradle publish $PROPS"
 fi
